51406183101a568209ed2127f1d07f0d6767448b
Ziiyodullayevv <akobir2323@gmail.com>
2023-03-04 07:57:51 +0500

    6-7-8 kyu

---
+++ 6-kyu/index.js
@@ -0,0 +1,13 @@
+// 1 - KATA - 6 kyu:
+// Asosiy g'oya qatordagi barcha uchraydigan belgilarni sanashdir. Agar sizda kabi satr "abc" bo'lsa , unda natija bo'lishi kerak {'a': 2, 'b': 1}.
+// Agar satr bo'sh bo'lsa-chi? Keyin natija bo'sh ob'ekt literal bo'lishi kerak {}.
+
+// YECHIM:
+function count(string) {
+  var count = {};
+  string.split("").map((s) => {
+    count[s] ? count[s]++ : (count[s] = 1);
+  });
+  return count;
+}
+//console.log(count(''));

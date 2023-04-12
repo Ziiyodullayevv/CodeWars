1bb8518c5ac6935a1a58dcdc48327a0dd6abe961
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-12 20:19:45 +0500

    8 kyu: 30 - kata

--- 8-kyu/index.js
+++ 8-kyu/index.js
@@ -615,6 +615,7 @@ const getTotal2 = (arr) => {
 
 // 3) usul:
 
+
 const getTotal3 = (arr) => {
   return arr.reduce((total, value) => total + value, 0);
 };
@@ -670,8 +671,26 @@ const isEqual = (arr1, arr2) => {
 };
 // console.log(isEqual(num1, num2));
 
-// --------------------------------------------------------------------
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// --------------------------------------------------------------------
+// 30 - KATA:
 
-// ===========================================
+function resolt(n) {
+  let str = String(n);
+  let res = '';
+
+  for (value of str) {
+    if (value === '0') res += ' nol';
+    else if (value === '1') res += ' bir';
+    else if (value === '2') res += ' ikki';
+    else if (value === '3') res += ' uch';
+    else if (value === '4') res += ' tort';
+    else if (value === '5') res += ' besh';
+    else if (value === '6') res += ' olti';
+    else if (value === '7') res += ' yetti';
+    else if (value === '8') res += ' sakkiz';
+    else if (value === '9') res += ' toqqiz';
+  }
+  return res.trim();
+}
+// console.log(resolt(123456));

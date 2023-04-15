acaea7e073419a9f32237a79529552b5f034458e
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-15 20:36:16 +0500

    31 - kata

--- 8-kyu/index.js
+++ 8-kyu/index.js
@@ -694,3 +694,18 @@ function resolt(n) {
   return res.trim();
 }
 // console.log(resolt(123456));
+
+
+
+// 31 - Kata:
+
+
+const getTubSon = (n) => {
+  for (let i = 2; i <= n; i++) {
+    if (i === 2 || i === 3 || i === 5) console.log(i);
+    else if (!(i % 5 == 0 || i % 3 === 0 || i % 2 === 0)) console.log(i);
+  }
+};
+console.log(getTubSon(100));
+
+

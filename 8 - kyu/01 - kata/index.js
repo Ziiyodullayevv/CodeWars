6d24139b4501fc3ac627e8876577f2a1cd639412
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-15 23:09:06 +0500

    N songacha bolgan sonlarni chiqaring

---
+++ 8 - kyu/01 - kata/index.js
@@ -0,0 +1,18 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// 1 - KATA:
+// (N) SON BERILGAN FUNKSIYA ORQALI (N)SONDAN  1 GACHA BOLGAN SONLARNI MASSIVDA QAYTARING?
+// OUTPUT: N = 5;  [5, 4, 3, 2, 1];
+
+// YECHIM:
+const reverseSeq = (n) => {
+    let arr = [];
+    for (let i = n; i > 0; i--) {
+      arr.push(i);
+    }
+    return arr;
+  };
+  
+  //  console.log(reverseSeq(5));
+  
+  // -----------------------------------------------------------------------------------------

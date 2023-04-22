13313f9d603bf52cda0b222f91d28b596253ed5a
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-23 00:22:54 +0500

    8kyu: Teskari tartiblang

---
+++ 020 - kata/problem.js
@@ -0,0 +1,16 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Ushbu katada siz ro'yxatni oladigan va teskari tartibda ro'yxatni qaytaradigan funksiya yaratasiz.
+
+// Output:
+
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1];
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9];
+
+function reverseList (list) {
+  return list.reverse ();
+}
+
+console.log (reverseList ([2, 4, 5, 6]));
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

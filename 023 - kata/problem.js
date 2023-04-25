5ae4984f6ddf99ae98d9ff850d4dd91acdab4c1e
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-25 21:30:24 +0500

    8kyu: Toq yoki Juft

---
+++ 023 - kata/problem.js
@@ -0,0 +1,16 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Funksiya yarating argument sifatida son kiritilganda u juft bolsa "Even" toq bolse "Odd"
+
+// OUTPUT:
+
+// evenOrOdd(8) -> 'Even';
+// evenOrOdd(7) -> 'Odd'
+
+function evenOrOdd (number) {
+  return number % 2 ? 'Odd' : 'Even';
+}
+
+//console.log(evenOrOdd(8));
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

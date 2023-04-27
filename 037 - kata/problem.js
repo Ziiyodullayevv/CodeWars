1752278188d3f5536df378779d2f2e9455f94a89
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-27 22:29:55 +0500

    8kyu: Matn so'zlarini teskari qilib yozing

---
+++ 037 - kata/problem.js
@@ -0,0 +1,12 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Matn berilgan shu matnni sozlarini teskari qilib yozib bering?
+
+// OUTPUT:
+// "Hello world" --> "world Hello"
+function reverse (string) {
+  return string.split (' ').reverse ().join (' ');
+}
+// console.log(reverse("Hello world"));
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

0492ffbe11deec90d9f9f3970a9463c594d07b7b
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-27 22:05:42 +0500

    8kyu: Berilgan raqamni satrga aylantiring

---
+++ 033 - kata/problem.js
@@ -0,0 +1,26 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// 1 dan 9 gacha bolgan sonlarni raqam emas balki string korinishidagi yani: misol 1 --> 'One' tarzida chiqaruvchi function yozing?
+
+// YECHIM:
+
+function switchItUp (number) {
+  let words = [
+    'Zero',
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    'Six',
+    'Seven',
+    'Eight',
+    'Nine',
+  ];
+  return words[number];
+}
+
+//console.log(switchItUp(8));
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+

81859b5db6ba959c6c8c873e6366a13386f5cbac
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-27 22:34:01 +0500

    Arraylarni bir-biri bilan taqqoslang

---
+++ 040 - kata/problem.js
@@ -0,0 +1,13 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Arrayni bir-biriga taqqoslang? Togridan togri taqqoslab bolmas ekanligini yodda tuting chunki array Obectdir!!!
+
+const num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const num2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const isEqual = (arr1, arr2) => {
+  return arr1.join ('') === arr2.join ('');
+};
+// console.log(isEqual(num1, num2));
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

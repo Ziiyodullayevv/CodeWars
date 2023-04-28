306657142f793e11b8906f42fc81166631689bcb
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-28 23:51:41 +0500

    8kyu: Manfiy va Musbat

---
+++ 044 - kata/problem.js
@@ -0,0 +1,16 @@
+// ________________________________________________________________
+
+// Juda oddiy, butun son yoki kasr son  berilganda, uning teskarisini cheqarib beradigan funksiya yozing.
+
+// Misollar:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite (number) {
+  return -number;
+}
+console.log (opposite (34));
+
+//________________________________________________________________
